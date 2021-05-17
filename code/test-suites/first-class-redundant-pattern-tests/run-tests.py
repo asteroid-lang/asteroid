eaa17ef038ce8cd98bfb43d2a86daf1f12d56d2b
TimColaneri <47124912+TimColaneri@users.noreply.github.com>
2021-05-17 17:30:14 -0400

    Added Redundant First-Class Patterns Testing Suite
    Installed around 30 Asteroid programs which utilize first-class patterns. Each of these programs has a redundant pattern that somehow involves a first-class pattern.
    
    Included is a small python script run-tests.py which will run all of these small test programs, printing their output to the console.
    
    User may have to update the sys.path variable in run-tests.py

---
+++ code/test-suites/first-class-redundant-pattern-tests/run-tests.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Regression Tests
+
+
+import sys
+import os
+
+# uncomment if tim
+sys.path[0] ='/Users/Timothy/Documents/GitHub/asteroid/code'
+
+# uncomment if running on SageMaker
+#sys.path[0] = '/home/ec2-user/SageMaker/asteroid/code'
+#sys.path[0] = '/Users/lutz/Dropbox/URI/Projects/Asteroid/asteroid-git/code'
+from asteroid_interp import interp
+
+programs = os.listdir("programs")
+programs.sort()
+
+for pname in programs:
+    f = open("programs/"+pname,"r")
+    p = f.read()
+    print("********************"+pname+"**********************")
+    print(p)
+    print("********************output*********************")
+    try:
+        interp(p)
+    except:
+        pass
+    f.close()
