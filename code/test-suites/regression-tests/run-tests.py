099089c815a85d7de90cfb67dbb8b24d5c80628d
olwmc <olwmc@protonmail.ch>
2021-05-26 15:36:38 -0400

    Added relative pathing to the regression tests.

--- code/test-suites/regression-tests/run-tests.py
+++ code/test-suites/regression-tests/run-tests.py
@@ -7,9 +7,15 @@
 import sys
 import os
 
-# uncomment if running on SageMaker
-#sys.path[0] = '/home/ec2-user/SageMaker/asteroid/code'
-sys.path[0] = '/Users/lutz/Dropbox/URI/Projects/Asteroid/asteroid-git/code'
+# Get the path of this run-tests.py file
+file_path = os.path.dirname(os.path.abspath( __file__ ))
+
+# Temporarly change the working directory to that path
+os.chdir(file_path)
+
+# Set the path to two levels above (the code directory)
+sys.path[0] = "../../"
+
 from asteroid_interp import interp
 
 programs = os.listdir("programs")
@@ -21,5 +27,5 @@ for pname in programs:
     print("**********"+pname+"************")
     print(p)
     print("**********output***********")
-    interp(p, exceptions=True)
+    interp(p)
     f.close()
