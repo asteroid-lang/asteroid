78091dd318aa8b98e02d72c01ae603864e76c268
Lutz Hamel <lutz.hamel@gmail.com>
2021-06-22 14:45:40 -0400

    Revert "edits"
    This reverts commit 3d2429115b68f209bfbd54cec2c07d3df0316808.

--- README.md
+++ README.md
@@ -165,15 +165,15 @@ structure Dog with
 
   -- member function
   function add_trick
-    with (self, new_trick) do
-      let self @tricks = self @tricks + [new_trick].
+    with new_trick do
+      let this @tricks = this @tricks + [new_trick].
     end
 
   -- constructor
   function __init__
-    with (self, name) do
-      let self @name = name.
-      let self @tricks = [].
+    with name do
+      let this @name = name.
+      let this @tricks = [].
     end
 
   end -- structure
---
+++ code/programs/run-all.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# run all program
+
+
+import sys
+import os
+import glob
+
+# Get the path of this run-tests.py file
+file_path = os.path.dirname(os.path.abspath( __file__ ))
+
+# Temporarly change the working directory to that path
+os.chdir(file_path)
+
+# Set the path to the code directory
+sys.path[0] = "../"
+
+from asteroid_interp import interp
+
+programs = glob.glob("*.ast")
+programs.sort()
+
+for pname in programs:
+    f = open(pname,"r")
+    p = f.read()
+    print("**********"+pname+"************")
+    print(p)
+    print("**********output***********")
+    interp(p)
+    f.close()
