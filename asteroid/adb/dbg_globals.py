e04d73f906cbc955400d99860106f1ebf4effc6c
olwmc <olwmc@protonmail.ch>
2022-06-14 20:14:00 -0400

    Beginning help menu

---
+++ asteroid/adb/dbg_globals.py
@@ -0,0 +1,43 @@
+command_description_table = {
+    'macro':        """[m]acro (name) (command list)
+    """,
+    
+    'step':         """[s]tep
+    """,
+    
+    'continue':     """[[c]ont]inue
+    """,
+    
+    'next':         """[n]ext
+    """,
+    
+    'break':        """[b]reak ?(number)
+    """,
+    
+    'print':        """[p]rint name
+    """,
+    
+    'delete':       """[[d]el]ete (number)
+    """,
+    
+    '!':            """!
+    """,
+    
+    'longlist':     """[ll] longlist
+    """,
+    
+    'list':         """[l]ist
+    """,
+    
+    'quit':         """[q]uit
+    """,
+    
+    'explicit':     """[e]xplicit
+    """,
+    
+    'unexplicit':   """[u]nexplicit
+    """,
+    
+    'help':         """[h]elp
+    """
+}
