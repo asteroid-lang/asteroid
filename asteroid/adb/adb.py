6a7ac0ad3a8924f13eb51b93e21979142ea8192a
olwmc <olwmc@protonmail.ch>
2022-08-12 18:59:23 -0400

    Added macro to string

--- asteroid/adb/adb.py
+++ asteroid/adb/adb.py
@@ -86,41 +86,6 @@ class ADB:
         # Old lineinfo to be stored when until command is used
         self.old_lineinfo = None
 
-    def macro_to_string(self, macro_name):
-        contents = self.macros[macro_name]
-
-        outstr = ""
-
-        for cmd in contents:
-            match(cmd):
-                case ('MACRO',):                    outstr += 'macro'
-                case ('EVAL', value):               outstr += 'eval("{}")'.format(value)
-                case ('BANG', ):                    outstr += '!'
-                case ('HELP', name):                outstr += 'help {}'.format(name)
-                case ('UP',):                       outstr += '<'
-                case ('DOWN',):                     outstr += '>'
-                case ('WHERE',):                    outstr += 'where'
-                case ('LONGLIST',):                 outstr += 'longlist'
-                case ('LIST',):                     outstr += 'list'
-                case ('RETVAL',):                   outstr += '__retval__'
-                case ('UNTIL',):                    outstr += 'until'
-                case ('RETURN',):                   outstr += 'return'
-                case ('EXPLICIT', set_explicit):    outstr += 'explicit {}'.format(set_explicit if set_explicit else "")
-                case ('STEP', ):                    outstr += 'step'
-                case ('CONTINUE', ):                outstr += 'continue'
-                case ('NEXT', ):                    outstr += 'next'
-
-                # TODO: (OWM) print out conditions for these
-                case ('BREAK', nums, conds):        outstr += "break {}".format(' '.join([str(n) for n in nums]))
-
-                case ('DELETE', nums):              outstr += "delete {}".format(' '.join([str(n) for n in nums]))
-                case ('NAME', v):                   outstr += str(v)
-                case ('QUIT', ):                    outstr += 'quit'
-                case ('NOOP', ):                    outstr += 'noop'
-
-            outstr += '; '
-
-        return outstr
 
     def reset_defaults(self):
         """
@@ -475,6 +440,42 @@ class ADB:
             print("* {} : {}".format(
                 m, self.macro_to_string(m)
             ))
+    
+    def macro_to_string(self, macro_name):
+        contents = self.macros[macro_name]
+
+        outstr = ""
+
+        for cmd in contents:
+            match(cmd):
+                case ('MACRO',):                    outstr += 'macro'
+                case ('EVAL', value):               outstr += 'eval("{}")'.format(value)
+                case ('BANG', ):                    outstr += '!'
+                case ('HELP', name):                outstr += 'help {}'.format(name)
+                case ('UP',):                       outstr += '<'
+                case ('DOWN',):                     outstr += '>'
+                case ('WHERE',):                    outstr += 'where'
+                case ('LONGLIST',):                 outstr += 'longlist'
+                case ('LIST',):                     outstr += 'list'
+                case ('RETVAL',):                   outstr += '__retval__'
+                case ('UNTIL',):                    outstr += 'until'
+                case ('RETURN',):                   outstr += 'return'
+                case ('EXPLICIT', set_explicit):    outstr += 'explicit {}'.format(set_explicit if set_explicit else "")
+                case ('STEP', ):                    outstr += 'step'
+                case ('CONTINUE', ):                outstr += 'continue'
+                case ('NEXT', ):                    outstr += 'next'
+
+                # TODO: (OWM) print out conditions for these
+                case ('BREAK', nums, conds):        outstr += "break {}".format(' '.join([str(n) for n in nums]))
+
+                case ('DELETE', nums):              outstr += "delete {}".format(' '.join([str(n) for n in nums]))
+                case ('NAME', v):                   outstr += str(v)
+                case ('QUIT', ):                    outstr += 'quit'
+                case ('NOOP', ):                    outstr += 'noop'
+
+            outstr += '; '
+
+        return outstr
 
     def set_new_macro(self, name, l):
         """
