4ee8b9755ebb9cdc5508977e61f3dd2dfd13f9b8
Nicholas Mendes <nicholas_mendes@uri.edu>
2024-04-06 16:28:09 -0400

    Address previous code fragility

--- asteroid/mad.py
+++ asteroid/mad.py
@@ -49,6 +49,8 @@
 
 from os.path import exists, split, basename
 from asteroid.support import term2string, get_tail_term, term2verbose, find_function
+from asteroid.frontend import stmt_lookahead, exp_lookahead
+from asteroid.lex import token_specs, keywords
 from asteroid.version import MAD_VERSION
 import copy 
 
@@ -411,6 +413,23 @@ class MAD:
          return START_DEBUGGER
    
    def _validate_breakpoint_line(self, fname, lineno):
+      # Extract only statement specific keywords
+      stmts = stmt_lookahead - exp_lookahead
+      stmt_kws, non_stmt_tks = set(), set()
+      prev_exceptions, next_exceptions = set(), set()
+      # Separate the statement and non statement keywords
+      for (val, ty) in keywords.items():
+         if ty in stmts:
+            stmt_kws.add(val)
+         else:
+            non_stmt_tks.add(val)
+      # Add operators and symbol characters, adding exceptions to lines where needed.
+      for (val, display, ty) in token_specs:
+         if ty == 'DOT':
+            prev_exceptions.add(display.strip("'"))
+         elif not display[0] == "'" and display[-1] == "'":
+            non_stmt_tks.add(display.strip("'"))
+      next_exceptions.update(['end', '[EOF]'])
       # Load the correct file
       file_text = self.program_text[fname]
       # If the line is outside the current file, produce an error message and return
@@ -423,26 +442,26 @@ class MAD:
          print("error: cannot place breakpoints on blank lines")
          return False
       # If the line starts with any of these tokens, it is a valid statement and can accept a breakpoint
-      for start in ['load', 'global', 'structure', 'let', 'loop', 'for', 'while', 'repeat', 'match', 'if', 'try', 'throw', 'break', 'return', 'function']:
+      for start in stmt_kws:
          if len(line) >= len(start) and line[:len(start)] == start:
             return True
       # Get the lines above and below the current if they exist
       prev = file_text[lineno-2].strip() if lineno > 1 else None
       next = file_text[lineno].strip() if lineno < len(file_text) else None
       # If the line starts with any of these tokens, the line is in the middle of a statement or expression and cannot accept a breakpoint
-      for start in ['system', 'as', 'with', 'end', 'do', 'in', 'until', 'elif', 'else', 'catch', '@', ',', '[', ']', '(', ')', '"', "'"]:
-         if len(line) >= len(start) and line[:len(start)] == start:
+      for start in non_stmt_tks:
+         if len(line) >= len(start) and line.split(' ')[0] == start:
             print("error: cannot place breakpoint inside a statement")
             return False
-      # If the previous or next lines begins with any of these symbols, the current line is in the middle of a statement
+      # If the previous line ends or next lines begins with any of these symbols, the current line is in the middle of a statement
       if prev:
-         for start in ['@', ',', '[', '(', '"', "'"]:
-            if len(prev) >= len(start) and prev[:len(start)] == start:
+         for start in non_stmt_tks:
+            if len(prev) >= len(start) and prev.split(' ')[-1] == start and start not in prev_exceptions:
                print("error: cannot place breakpoint inside a statement")
                return False
       if next:
-         for start in ['@', ',', ']', ')', '"', "'"]:
-            if len(next) >= len(start) and next[:len(start)] ==  start:
+         for start in non_stmt_tks:
+            if len(next) >= len(start) and next.split(' ')[0] == start and start not in next_exceptions:
                print("error: cannot place breakpoint inside a statement")
                return False
       # If it cannot be determined, assume the breakpoint is valid
